da42ecbca70752841e2fdc17facb646893f2605e
Gavin Garcia <edwardgavingarcia@yahoo.com>
2024-01-11 19:41:56 -0700

    Create domUpdates file/moved promise.all

--- src/scripts.js
+++ src/scripts.js
@@ -2,22 +2,4 @@
 import "./css/styles.css";
 import "./images/turing-logo.png";
 import "./apiCalls";
-import { fetchAPIcall } from "./apiCalls";
-
-// Created the intial structure of Promise.all to run on load so the data is populated initially.
-// Unsure of how the specific user by login will function with it so will assign a const id to 1 to get that specified user.
-window.addEventListener("load", function () {
-  const id = 1;
-  Promise.all([
-    fetchAPIcall(`customers/${id}`),
-    fetchAPIcall("customers"),
-    fetchAPIcall("rooms"),
-    fetchAPIcall("bookings"),
-  ]).then((allData) => {
-    console.log(allData);
-    // currentCustomer = allData[0];
-    // allCustomers = allData[1].customers;
-    // allRooms = allData[2].rooms;
-    // allBookings = allData[3].bookings;
-  });
-});
+import "./domUpdates";
