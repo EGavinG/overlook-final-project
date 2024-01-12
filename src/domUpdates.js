77d7ab789ad14e20fbcf62ee15a59f9a248e4eae
Gavin Garcia <edwardgavingarcia@yahoo.com>
2024-01-11 21:39:41 -0700

    Created customers.js/ customer data functions/disperseAllData function

--- src/domUpdates.js
+++ src/domUpdates.js
@@ -1,11 +1,18 @@
+// Imports
 import { fetchAPIcall } from "./apiCalls";
+import {
+  currentCustomersBookedRooms,
+  currentCustomersRoomInfo,
+  customersTotalSpending,
+} from "./customers";
 
+// Global Variables
 let currentCustomer;
 let allCustomers;
 let allRooms;
 let allBookings;
 
-// Created the intial structure of Promise.all to run on load so the data is populated initially.
+// EventListeners
 // Unsure of how the specific user by login will function with it so will assign a const id to 1 to get that specified user.
 window.addEventListener("load", function () {
   const id = 1;
@@ -15,12 +22,23 @@ window.addEventListener("load", function () {
     fetchAPIcall("rooms"),
     fetchAPIcall("bookings"),
   ]).then((allData) => {
-    console.log(allData);
     currentCustomer = allData[0];
     allCustomers = allData[1].customers;
     allRooms = allData[2].rooms;
     allBookings = allData[3].bookings;
+    disperseAllData(currentCustomer, allCustomers, allRooms, allBookings);
   });
 });
 
-export { currentCustomer, allCustomers, allRooms, allBookings };
+// Dom Functions
+const disperseAllData = () => {
+  const customersBookedRooms = currentCustomersBookedRooms(
+    currentCustomer,
+    allBookings
+  );
+  const customersRoomInfo = currentCustomersRoomInfo(
+    customersBookedRooms,
+    allRooms
+  );
+  const customersBookingsExpense = customersTotalSpending(customersRoomInfo);
+};
