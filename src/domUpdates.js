0a2dfee94a2b094c4a0c67d4d55e9e8be196ba87
Gavin Garcia <edwardgavingarcia@yahoo.com>
2024-01-17 02:23:37 -0700

    Alphabetize functions

--- src/domUpdates.js
+++ src/domUpdates.js
@@ -1,88 +1,8 @@
 // Imports
-import { fetchAPIcall, bookRoom } from "./apiCalls";
-import {
-  customersBookingsInfo,
-  customersTotalSpending,
-  uniqueRoomTypes,
-} from "./customers";
+import { customersBookingsInfo, customersTotalSpending, uniqueRoomTypes } from "./customers";
 
-// Global Variables
-let allRooms;
-let allBookings;
-
-// Query Selectors
-const roomDetailsList = document.getElementById("roomDetailsList");
-const availableRoomsList = document.getElementById("availableRooms");
-const roomTypeFilter = document.getElementById("roomTypeFilter");
-const calendarError = document.getElementById("calendarError");
-const bookStayText = document.getElementById("bookStayText");
-const searchRoomsButton = document.getElementById("searchRooms");
-
-// EventListeners
-
-
-function handleFormSubmit(e) {
-  e.preventDefault();
-  const fromDate = fromDateInput.value;
-  const selectedRoomType = roomTypeFilter.value;
-
-  if (fromDate) {
-    const roomsConnectedToDate = availableRooms(date);
-
-    const filteredRooms =
-      selectedRoomType !== "roomOptions"
-        ? filterRoomsByType(roomsConnectedToDate, selectedRoomType)
-        : roomsConnectedToDate;
-
-    displayBookedRoomInfo(filteredRooms);
-  } else {
-    calendarError.classList.remove("hidden");
-    bookStayText.classList.add("hidden");
-  }
-}
-
-// Dom Functions
-const displayCustomerData = (customer, rooms, bookings) => {
-  displayWelcomeHeader(customer);
-
-  const bookedRoomsInfo = customersBookingsInfo(customer, rooms, bookings);
-  displayTotalAmountSpent(bookedRoomsInfo)
-  displayBookedRoomsInfo(bookedRoomsInfo);
-};
-
-function displayWelcomeHeader(customer) {
-  header.textContent = `Welcome to Your Overlook Booking Dashboard, ${customer.name.split(" ")[0]}!`;
-};
-
-function displayTotalAmountSpent(bookedRoomsInfo) {
-  const spendingInfoContainer = document.querySelector("p.spent");
-  const amountSpent = customersTotalSpending(bookedRoomsInfo);
-
-  spendingInfoContainer.innerText = `Total Amount Spent: $${amountSpent}`;
-}
-
-function filterRoomsByType(rooms, roomType) {
-  return rooms.filter((room) => room.roomType === roomType);
-}
-
-// Function that updates the dom to show filtered room results
-const displayRooms = (rooms) => {
-
-}
-
-const displayBookedRoomsInfo = (bookedRoomsInfo) => {
-  availableRoomsList.innerHTML = ""; // Clear filtered room details list
-  roomDetailsList.innerHTML = ""; // Clear original room details list
-
-  bookedRoomsInfo.forEach((bookedRoomInfo) => {
-    const listItem = document.createElement("li");
-    listItem.textContent = `Date: ${bookedRoomInfo.date}, Info Type: ${bookedRoomInfo.roomType}, Bed Size: ${bookedRoomInfo.bedSize}, Cost per Night: $${bookedRoomInfo.costPerNight}`;
-    roomDetailsList.appendChild(listItem);
-  });
-};
-
-// Dom function that updates filtered results
 const displayAvailableRooms = (availableRooms) => {
+  const availableRoomsList = document.getElementById("availableRooms");
 
   if (availableRooms.length === 0) {
     availableRoomsList.innerHTML = `<p>No rooms available with the selected criteria. Please search again.</p>`;
@@ -114,7 +34,37 @@ const displayAvailableRooms = (availableRooms) => {
   };
 };
 
-// provides room options
+const displayBookedRoomsInfo = (bookedRoomsInfo) => {
+  const roomDetailsList = document.getElementById("roomDetailsList");
+
+  roomDetailsList.innerHTML = ""; // Clear original room details list
+
+  bookedRoomsInfo.forEach((bookedRoomInfo) => {
+    const listItem = document.createElement("li");
+    listItem.textContent = `Date: ${bookedRoomInfo.date}, Info Type: ${bookedRoomInfo.roomType}, Bed Size: ${bookedRoomInfo.bedSize}, Cost per Night: $${bookedRoomInfo.costPerNight}`;
+    roomDetailsList.appendChild(listItem);
+  });
+};
+
+const displayCustomerData = (customer, rooms, bookings) => {
+  displayWelcomeHeader(customer);
+
+  const bookedRoomsInfo = customersBookingsInfo(customer, rooms, bookings);
+  displayTotalAmountSpent(bookedRoomsInfo)
+  displayBookedRoomsInfo(bookedRoomsInfo);
+};
+
+function displayTotalAmountSpent(bookedRoomsInfo) {
+  const spendingInfoContainer = document.querySelector("p.spent");
+  const amountSpent = customersTotalSpending(bookedRoomsInfo);
+
+  spendingInfoContainer.innerText = `Total Amount Spent: $${amountSpent}`;
+};
+
+function displayWelcomeHeader(customer) {
+  header.textContent = `Welcome to Your Overlook Booking Dashboard, ${customer.name.split(" ")[0]}!`;
+};
+
 const updateRoomTypeFilterOptions = (rooms) => {
   const roomTypeFilter = document.getElementById("roomTypeFilter");
   roomTypeFilter.innerHTML = "";
@@ -136,4 +86,4 @@ const updateRoomTypeFilterOptions = (rooms) => {
   });
 };
 
-export { displayCustomerData, updateRoomTypeFilterOptions, displayAvailableRooms };
+export { displayAvailableRooms, displayCustomerData, updateRoomTypeFilterOptions };
